ff965e8c724f1a45921cf0c9b47b1fbe9183e24e
rgb-keys <ramongtstudy@gmail.com>
2025-10-10 16:16:12 -0300

    feat: add client and user base entity dto

---
+++ src/api/presentation/dtos/client/client.entity-dto.ts
@@ -0,0 +1,52 @@
+import { ApiProperty, OmitType } from '@nestjs/swagger'
+import { IncomeBaseEntity } from '../income/income.entity-dto'
+import { UserBaseEntity } from '../user/user.entity-dto'
+
+export class ClientBaseEntity {
+	@ApiProperty({
+		type: 'string',
+		format: 'uuid',
+	})
+	id: string
+
+	@ApiProperty({
+		type: 'string',
+	})
+	name: string
+
+	@ApiProperty({
+		type: 'string',
+		nullable: true,
+	})
+	phoneNumber?: number | null
+
+	@ApiProperty({
+		type: 'number',
+		format: 'decimal',
+		nullable: true,
+	})
+	monthlyIncome?: number | null
+
+	@ApiProperty({
+		type: 'string',
+		format: 'date-time',
+	})
+	createdAt: Date
+
+	@ApiProperty({
+		type: 'string',
+		format: 'date-time',
+	})
+	updatedAt: Date
+}
+
+export class ClientResponseEntity extends OmitType(UserBaseEntity, [
+	'passwordHash',
+]) {}
+
+export class ClientIncomeResponseEntity extends IncomeBaseEntity {}
+
+export class ClientMonthlyIncomeResponseEntity extends OmitType(
+	ClientBaseEntity,
+	['createdAt', 'updatedAt', 'name', 'phoneNumber'],
+) {}
