98b2458f401d2122f6d38d9f53e5220f30a719e4
rgb-keys <ramongtstudy@gmail.com>
2025-10-10 15:55:59 -0300

    refactor: change import searchableFields and SummaryDTO

--- src/api/application/repositories/income.repository.ts
+++ src/api/application/repositories/income.repository.ts
@@ -1,20 +1,20 @@
 import { Income } from '@/api/domain/entities/income.entity'
 import { OutputCollectionDTO } from '@/shared/core/dtos/output-collection-dto'
 import { SearchParams } from '@shared'
-import { ExpenseSummaryDTO } from '../dtos/expense.dto'
+import { IncomeSummaryDTO } from '../dtos/income.dto'
 
 export abstract class IncomeRepository {
 	abstract findUnique(params: FindUniqueIncomeParams): Promise<Income | null>
 	abstract listSummary(
-		params?: SearchParams<ExpenseSearchableFields>,
-	): Promise<OutputCollectionDTO<ExpenseSummaryDTO>>
+		params?: SearchParams<IncomeSearchableFields>,
+	): Promise<OutputCollectionDTO<IncomeSummaryDTO>>
 }
 
 export interface FindUniqueIncomeParams {
 	incomeId?: string
 }
 
-export interface ExpenseSearchableFields {
+export interface IncomeSearchableFields {
 	clientId: string
 	amount: number
 	date: Date
