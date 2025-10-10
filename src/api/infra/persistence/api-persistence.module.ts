9c567c6d5eb9d56883075732ff52094b774c47ea
rgb-keys <ramongtstudy@gmail.com>
2025-10-10 15:57:23 -0300

    feat: implement api persitence module

---
+++ src/api/infra/persistence/api-persistence.module.ts
@@ -0,0 +1,39 @@
+import { ClientRepository } from '@/api/application/repositories/client.repository'
+import { ExpenseRepository } from '@/api/application/repositories/expense.repository'
+import { GoalRepository } from '@/api/application/repositories/goal.repository'
+import { IncomeRepository } from '@/api/application/repositories/income.repository'
+import { DatabaseModule } from '@/shared/infra/database/database.module'
+import { Module } from '@nestjs/common'
+import { PrismaClientRepository } from './prisma/prisma-clients.repository'
+import { PrismaExpenseRepository } from './prisma/prisma-expenses.repository'
+import { PrismaGoalRepository } from './prisma/prisma-goals.repository'
+import { PrismaIncomeRepository } from './prisma/prisma-incomes.repository'
+
+@Module({
+	imports: [DatabaseModule],
+	providers: [
+		{
+			provide: ClientRepository,
+			useClass: PrismaClientRepository,
+		},
+		{
+			provide: IncomeRepository,
+			useClass: PrismaIncomeRepository,
+		},
+		{
+			provide: ExpenseRepository,
+			useClass: PrismaExpenseRepository,
+		},
+		{
+			provide: GoalRepository,
+			useClass: PrismaGoalRepository,
+		},
+	],
+	exports: [
+		ClientRepository,
+		IncomeRepository,
+		ExpenseRepository,
+		GoalRepository,
+	],
+})
+export class ApiPersistenceModule {}
