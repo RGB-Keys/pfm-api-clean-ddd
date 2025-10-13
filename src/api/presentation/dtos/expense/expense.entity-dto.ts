5caca61d211fa3f91cc63ab9200d71209c9a9dd6
rgb-keys <ramongtstudy@gmail.com>
2025-10-13 16:15:47 -0300

    feat: expense base entity, get and list dto

---
+++ src/api/presentation/dtos/expense/expense.entity-dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class ExpenseBaseEntity {
+	@ApiProperty({
+		type: 'string',
+		format: 'uuid',
+	})
+	id: string
+
+	@ApiProperty({
+		type: 'number',
+		format: 'decimal',
+	})
+	amount: number
+
+	@ApiProperty({
+		type: 'string',
+		format: 'date-time',
+	})
+	date: Date
+
+	@ApiProperty({
+		type: 'string',
+		nullable: true,
+	})
+	description?: string
+
+	@ApiProperty({
+		type: 'string',
+		nullable: true,
+	})
+	category?: string
+
+	@ApiProperty({
+		type: 'string',
+		format: 'date-time',
+	})
+	updatedAt: Date
+}
