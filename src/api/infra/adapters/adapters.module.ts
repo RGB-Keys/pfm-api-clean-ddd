045ba7f6a4ae4fb8b61deee1702f3b1b67345bfe
rgb-keys <ramongtstudy@gmail.com>
2025-10-13 16:12:44 -0300

    feat: implement ListUseCase inject

--- src/api/infra/adapters/adapters.module.ts
+++ src/api/infra/adapters/adapters.module.ts
@@ -3,11 +3,26 @@ import { CryptographyModule } from '../http/cryptography/cryptography.module'
 import { ApiPersistenceModule } from '../persistence/api-persistence.module'
 import { EventsModule } from './events/event.module'
 import { ClientAdapter } from './use-cases/client.adapter'
+import { ExpenseAdapter } from './use-cases/expense.adapter'
+import { GoalAdapter } from './use-cases/goal.adapter'
+import { IncomeAdapter } from './use-cases/income.adapter'
 import { UserAdapter } from './use-cases/user.adapter'
 
 @Module({
 	imports: [ApiPersistenceModule, CryptographyModule, EventsModule],
-	providers: [...UserAdapter, ...ClientAdapter],
-	exports: [...UserAdapter, ...ClientAdapter],
+	providers: [
+		...UserAdapter,
+		...ClientAdapter,
+		...IncomeAdapter,
+		...ExpenseAdapter,
+		...GoalAdapter,
+	],
+	exports: [
+		...UserAdapter,
+		...ClientAdapter,
+		...IncomeAdapter,
+		...ExpenseAdapter,
+		...GoalAdapter,
+	],
 })
 export class AdaptersModule {}
--- src/api/infra/adapters/use-cases/client.adapter.ts
+++ src/api/infra/adapters/use-cases/client.adapter.ts
@@ -1,4 +1,6 @@
 import { ClientRepository } from '@/api/application/repositories/client.repository'
+import { CreateExpenseUseCase } from '@/api/application/use-cases/client/create-expense.use-case'
+import { CreateGoalUseCase } from '@/api/application/use-cases/client/create-goal.use-case'
 import { CreateIncomeUseCase } from '@/api/application/use-cases/client/create-income.use-case'
 import { SetMonthlyIncomeUseCase } from '@/api/application/use-cases/client/set-monthly-income.use-case'
 import { EventBus } from '@/shared/core/events/event-bus'
@@ -15,6 +17,18 @@ export const ClientAdapter: Provider[] = [
 		provide: CreateIncomeUseCase,
 		useFactory: (clientRepository: ClientRepository, eventBus: EventBus) =>
 			new CreateIncomeUseCase(clientRepository, eventBus),
+		inject: [ClientRepository, EventBus],
+	},
+	{
+		provide: CreateExpenseUseCase,
+		useFactory: (clientRepository: ClientRepository, eventBus: EventBus) =>
+			new CreateExpenseUseCase(clientRepository, eventBus),
+		inject: [ClientRepository, EventBus],
+	},
+	{
+		provide: CreateGoalUseCase,
+		useFactory: (clientRepository: ClientRepository, eventBus: EventBus) =>
+			new CreateGoalUseCase(clientRepository, eventBus),
 		inject: [ClientRepository, EventBus],
 	},
 ]
